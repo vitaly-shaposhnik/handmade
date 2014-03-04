e0eadd156eea6bb5622525a92312f92326f3f885
Vitaly Shaposhnik <vitaly.shaposhnik@gmail.com>
2014-03-04 23:36:29 +0200

    Add fixtures

---
+++ src/Acme/HandmadeBundle/DataFixtures/ORM/ImagesVault/LoadProductData.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace Acme\HandmadeBundle\DataFixtures\ORM;
+
+use Doctrine\Common\DataFixtures\FixtureInterface;
+use Doctrine\Common\Persistence\ObjectManager;
+
+use Acme\HandmadeBundle\Entity\Product;
+
+class LoadProductData implements FixtureInterface
+{
+    /**
+     * {@inheritDoc}
+     */
+    public function load(ObjectManager $manager)
+    {
+        $category = $manager
+            ->getRepository('AcmeHandmadeBundle:Category')
+            ->findOneBy(array('id'=>1))
+        ;
+
+        $product = new Product();
+        $product->setName('Открытка 1');
+        $product->setCategory($category);
+        $product->setDescription("описание");
+        $product->setImage(1);
+        $product->setPrice(1000);
+        $product->setSku('12321323424');
+        $product->setAnnotation('аннотация');
+        $product->setQuantity(20);
+
+        $manager->persist($product);
+
+        $manager->flush();
+    }
+}
---
+++ src/Acme/HandmadeBundle/DataFixtures/ORM/LoadImageData.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace Acme\HandmadeBundle\DataFixtures\ORM;
+
+use Doctrine\Common\DataFixtures\FixtureInterface;
+use Doctrine\Common\Persistence\ObjectManager;
+use Symfony\Component\HttpFoundation\File\File;
+
+use Acme\HandmadeBundle\Entity\Image;
+
+class LoadImageData implements FixtureInterface
+{
+    /**
+     * {@inheritDoc}
+     */
+    public function load(ObjectManager $manager)
+    {
+        copy(__DIR__.'/ImagesVault/1.jpg', __DIR__.'/Images/1.jpg');
+        copy(__DIR__.'/ImagesVault/2.jpg', __DIR__.'/Images/2.jpg');
+        copy(__DIR__.'/ImagesVault/3.jpg', __DIR__.'/Images/3.jpg');
+
+        $img1 = new File(__DIR__.'/Images/1.jpg');
+        $image1 = new Image();
+        $image1->setName('Name1');
+        $image1->imageFile = $img1;
+        $image1->evaluateUpload();
+        $manager->persist($image1);
+
+        $img2 = new File(__DIR__.'/Images/2.jpg');
+        $image2 = new Image();
+        $image2->setName('Name2');
+        $image2->imageFile = $img2;
+        $image2->evaluateUpload();
+        $manager->persist($image2);
+
+
+        $img3 = new File(__DIR__.'/Images/3.jpg');
+        $image3 = new Image();
+        $image3->setName('Name3');
+        $image3->imageFile = $img3;
+        $image3->evaluateUpload();
+        $manager->persist($image3);
+
+        $manager->flush();
+    }
+}

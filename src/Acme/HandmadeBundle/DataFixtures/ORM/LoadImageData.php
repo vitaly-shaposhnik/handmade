06f18efc8268215354cc027b6ff450a50825a146
Vitaly Shaposhnik <vitaly.shaposhnik@gmail.com>
2014-03-29 13:27:32 +0200

    Fixtures

--- src/Acme/HandmadeBundle/DataFixtures/ORM/LoadImageData.php
+++ src/Acme/HandmadeBundle/DataFixtures/ORM/LoadImageData.php
@@ -15,42 +15,15 @@ class LoadImageData implements FixtureInterface
      */
     public function load(ObjectManager $manager)
     {
-        copy(__DIR__.'/ImagesVault/empty-product.png', __DIR__.'/Images/empty-product.png');
+        copy(__DIR__.'/ImagesVault/empty.png', __DIR__.'/Images/empty.png');
 
-        $img1 = new File(__DIR__.'/Images/empty-product.png');
+        $img1 = new File(__DIR__.'/Images/empty.png');
         $image1 = new Image();
-        $image1->setName('Name1');
+        $image1->setName('Empty image');
         $image1->imageFile = $img1;
-        $image1->evaluateUpload('empty-product');
+        $image1->evaluateUpload('empty');
         $manager->persist($image1);
-        $manager->flush();
 
-//        copy(__DIR__.'/ImagesVault/1.jpg', __DIR__.'/Images/1.jpg');
-//        copy(__DIR__.'/ImagesVault/2.jpg', __DIR__.'/Images/2.jpg');
-//        copy(__DIR__.'/ImagesVault/3.jpg', __DIR__.'/Images/3.jpg');
-//
-//        $img1 = new File(__DIR__.'/Images/1.jpg');
-//        $image1 = new Image();
-//        $image1->setName('Name1');
-//        $image1->imageFile = $img1;
-//        $image1->evaluateUpload();
-//        $manager->persist($image1);
-//
-//        $img2 = new File(__DIR__.'/Images/2.jpg');
-//        $image2 = new Image();
-//        $image2->setName('Name2');
-//        $image2->imageFile = $img2;
-//        $image2->evaluateUpload();
-//        $manager->persist($image2);
-//
-//
-//        $img3 = new File(__DIR__.'/Images/3.jpg');
-//        $image3 = new Image();
-//        $image3->setName('Name3');
-//        $image3->imageFile = $img3;
-//        $image3->evaluateUpload();
-//        $manager->persist($image3);
-//
-//        $manager->flush();
+        $manager->flush();
     }
 }

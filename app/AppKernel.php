c91224b92e49f6bcc4de27835a826f1f46598bdb
Vitaly Shaposhnik <vitaly.shaposhnik@gmail.com>
2014-04-20 20:57:26 +0300

    Try loggly

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -32,8 +32,6 @@ class AppKernel extends Kernel
             new FOS\RestBundle\FOSRestBundle(),
             new JMS\SerializerBundle\JMSSerializerBundle(),
             new Nelmio\ApiDocBundle\NelmioApiDocBundle(),
-
-            new Whitewashing\Bundle\LogglyBundle\WhitewashingLogglyBundle(),
         );
 
         if (in_array($this->getEnvironment(), array('dev', 'test'))) {
